05407a7e4839bca03f3da939622f054bf0da3a75
Michael JasonSmith <mpj17@onlinegroups.net>
2015-12-17 12:13:35 -0800

    Updating the product metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,38 @@
 # -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2011, 2012, 2013, 2014, 2015 E-Democracy.org and
+# Contributors.
+#
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.auth.oauth.client'
 version = get_version()
 
-setup(name='gs.auth.oauth.client',
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
+setup(
+    name=name,
     version=version,
     description="outh2 client registration methods",
-    long_description=open("README.rst").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.rst")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -17,22 +40,25 @@ setup(name='gs.auth.oauth.client',
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='outh2, client, registration',
     author='Richard Waid',
     author_email='richard@onlinegroups.net',
-    url='http://groupserver.org',
+    maintainer='Michael JasonSmith',
+    maintainer_email='mpj17@onlinegroups.net',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.auth', 'gs.auth.oauth'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'setuptools',
-        # --=mpj17=-- No, really, that is it.
+        'simplejson',
     ],
     entry_points="""
     # -*- Entry points: -*-
